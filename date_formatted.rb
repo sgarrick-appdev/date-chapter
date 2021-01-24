07925aa09c679ceae59f565f8015a52880dde551
sgarrick <sgarrick@uchicago.edu>
2021-01-24 23:31:35 +0000

    post grade

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,9 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2020, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+d = Date.today
+
+p "The year is: #{d.year}, the calendar day is: #{d.day}, and the month is: #{d.month}."
--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,7 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+
+require "date"
+days = Date.today - Date.new(1995,12,21)
+p "Ruby is #{days.to_i} days old!"
